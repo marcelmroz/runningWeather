a2189c46875140440161f0772a0c8cfd822f5677
Marcel Mroz <mmroz.marcel@gmail.com>
2023-07-19 19:54:22 +0200

    minor change

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
+const path = require("path");
 require('dotenv').config();
 
 console.log(process.env);
@@ -8,41 +9,36 @@ console.log(process.env);
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.set('views', path.join(__dirname, 'views'));
 
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/index.html"));
+});
 
-app.get("/",function(req,res){
-
-    res.sendFile(__dirname + "/index.html");
-})
-
-app.post("/", function(req,res) {
+app.post("/submit", function(req, res) {
     const query = req.body.cityName;
     const apiKey = process.env.API_KEY;
     const units = "metric";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&APPID=${apiKey}&units=${units}`;
 
-    https.get(url, function(respose){
-        console.log(respose.statusCode);
-        respose.on("data", function(data){
-        const weatherData = JSON.parse(data);
-        // console.log(weatherData);
-        
-        const tempDescription = weatherData.weather[0].description;
-        const temp = weatherData.main.temp;
-        const icon = weatherData.weather[0].icon;
-        const iconHttp = `http://openweathermap.org/img/wn/${icon}@2x.png`;
-        // console.log(temp);
-        res.write(`<p>The weather in ${query} is: ${tempDescription}</p>`);
-        res.write(`<h1>Temperature in ${query} is ${temp} degrees Celcius</h1>`);
-        res.write(`<img src =${iconHttp}>`);
-        res.send();
-        // res.sendFile(iconHttp);
-        })
+    https.get(url, function(response) {
+        console.log(response.statusCode);
+        response.on("data", function(data) {
+            const weatherData = JSON.parse(data);
+            const tempDescription = weatherData.weather[0].description;
+            const temp = weatherData.main.temp;
+            const icon = weatherData.weather[0].icon;
+            const iconHttp = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+
+            res.render('result.ejs', {
+                iconHttp_: iconHttp,
+                tempDescription_: tempDescription,
+                temp_: temp,
+                query_: query
+            });
+        });
     });
 });
-
-
-
 
 
 app.listen(3000, function(){
