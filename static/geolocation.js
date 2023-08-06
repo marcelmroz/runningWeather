8623eba31ad6e0bd714937d346a6e3ed458e7d00
Marcel Mroz <mmroz.marcel@gmail.com>
2023-08-06 20:00:45 +0200

    clean up

--- static/geolocation.js
+++ static/geolocation.js
@@ -1,10 +1,10 @@
 $(() => {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
+        navigator.geolocation.getCurrentPosition(function(position) {
             document.getElementById("latitudeInput").value = position.coords.latitude;
             document.getElementById("longitudeInput").value = position.coords.longitude;
             // console.log(position.coords.latitude + " " + position.coords.longitude);
-        }, function (error) {
+        }, function(error) {
             console.log("Geolocation error:", error.message);
         });
     } else {
